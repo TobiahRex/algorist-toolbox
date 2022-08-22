6fb5e1f3ded5dc0f5ffa31db36a40c98c642606e
Tobiah Rex <tobiah.rex@gmail.com>
2022-08-22 12:27:13 -0500

    Adds Sorting Algo Notes
    --------------
      WARNING: This commit is dirty
    
    --------------
      MAIN
      * Adds various sorting Theory Notes from lectures/resources.
      * Adds various sorting algorithms, with their respective notes,
      explanations.
    --------------
      NOTES
      *
    --------------

--- main.py
+++
@@ -1,8 +0,0 @@
-from graph_complexity import graph
-from algorithms.sort import quick_sort
-import random
-
-test_cases = []
-for i in range(0, 100):
-    test_cases.append([random.randint(0,1000) for i in range(random.randint(0,10000))])
-results = graph(quick_sort, test_cases, save_file=True)
